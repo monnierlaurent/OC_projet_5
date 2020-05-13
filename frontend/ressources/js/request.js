bb28be589126e815588cf41f19899961a59b19ca
monnierlaurent <laurent.monnier51@orange.fr>
2020-05-13 14:47:20 +0200

    promesse fetch() et affichage de tous les produits sur insex.html

--- frontend/ressources/js/request.js
+++ frontend/ressources/js/request.js
@@ -1,15 +1,33 @@
-var request = new XMLHttpRequest();
+fetch('http://localhost:3000/api/teddies/')
+    .then(function(response) {
+        return response.json();
 
-request.onreadystatechange = function() {
+    }).then(function(rep) {
 
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
+        for (i = 0; i < rep.length; i++) {
 
-        console.log(response[0].name + ' ' + response[0].description);
+            let sectionProduit = document.getElementById('page_items');
 
-    };
+            const newArticle = document.createElement('article');
+            newArticle.setAttribute('class', 'bloc__section__article--border');
+            sectionProduit.appendChild(newArticle);
 
-};
+            const newLien = document.createElement('a');
+            newLien.setAttribute('class', 'bloc__section__article--flex');
+            newLien.setAttribute('href', '#');
+            newArticle.appendChild(newLien);
 
-request.open("GET", "http://localhost:3000/api/teddies");
-request.send();
+            const newImg = document.createElement('img');
+            newImg.setAttribute('class', 'bloc__section__article--image');
+            newImg.setAttribute('src', rep[i].imageUrl);
+            newLien.appendChild(newImg);
+
+            const newName = document.createElement('h2');
+            newName.setAttribute('class', 'bloc__section__heading--font');
+            newName.setAttribute('src', '/backend/images/teddy_1.jpg');
+            newName.innerHTML = rep[i].name;
+            newLien.appendChild(newName);
+
+            console.log(rep[i]);
+        };
+    });
