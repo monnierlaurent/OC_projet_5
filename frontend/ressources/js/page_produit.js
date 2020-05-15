c27e1f5fd41f3c314cf85dfe322ccc8e92306971
monnierlaurent <laurent.monnier51@orange.fr>
2020-05-15 21:44:17 +0200

    affichage en dynamique de la page produit avec choix des couleurs

--- frontend/ressources/js/page_produit.js
+++ frontend/ressources/js/page_produit.js
@@ -1,41 +1,63 @@
-let urlJson = 'http://localhost:3000/api/teddies/';
+//let urlJson = 'http://localhost:3000/api/teddies/';
 
-const afficherproduits = async function() {
+const hash = window.location.hash; // On créé une variable "hash" qui correspond au hash de l'URL qui a été chargée par l'utilisateur
+const id = hash.replace("#", ""); // On reformate le "hash" pour lui enlever le symbole "#"
+const url = 'http://localhost:3000/api/teddies/' + id; // L'URL chargée sera celle corresponda
+
+
+const afficherproduits = async function(data2) {
 
     try {
-        let response = await fetch(urlJson);
+
+        let response = await fetch(url);
+
 
         if (response.ok) {
 
-            let data = await response.json().then(function(data, i) {
+            let data = await response.json().then(function(data2, i) {
 
-                for (i = 0; i < data.length; i++) {
+                let header = document.getElementById("nom_produit");
+                const newName = document.createElement('h2');
+                newName.setAttribute('class', 'bloc__section__header--font');
+                header.appendChild(newName);
+                newName.innerHTML = data2.name;
 
-                    let header = document.getElementById("nom_produit");
-                    const newName = document.createElement('h2');
-                    newName.setAttribute('class', 'bloc__section__header--font');
-                    header.appendChild(newName);
-                    newName.innerHTML = data[i].name;
+                let image = document.getElementById("image_produit");
+                const newImage = document.createElement('img');
+                newImage.setAttribute('class', 'bloc__section_2__img--seize');
+                newImage.setAttribute('src', data2.imageUrl);
+                newImage.setAttribute('alt', "photo de l'ourson");
+                image.appendChild(newImage);
+                newImage.innerHTML = data2.description;
 
-                    let image = document.getElementById("image_produit");
-                    const newImage = document.createElement('img');
-                    newImage.setAttribute('class', 'bloc__section_2__img--seize');
-                    newImage.setAttribute('src', data[i].imageUrl);
-                    newImage.setAttribute('alt', "photo de l'ourson");
-                    image.appendChild(newImage);
-                    newImage.innerHTML = data[i].description;
+                let paragraphe = document.getElementById("description_produit");
+                const newDescription = document.createElement('p');
+                paragraphe.appendChild(newDescription);
+                newDescription.innerHTML = data2.description;
 
-                    let paragraphe = document.getElementById("description_produit");
-                    const newDescription = document.createElement('p');
-                    paragraphe.appendChild(newDescription);
-                    newDescription.innerHTML = data[i].description;
+                const newPrice = document.createElement('p');
+                paragraphe.appendChild(newPrice);
+                newPrice.innerHTML = '<br>' + data2.price + '€';
 
-                    const newPrice = document.createElement('p');
-                    paragraphe.appendChild(newPrice);
-                    newPrice.innerHTML = '<br>' + data[i].price + '€';
+                let divSelect = document.getElementById("select_color");
+                const newLabel = document.createElement('label');
+                newLabel.setAttribute('class', 'bloc__section_2__label--font');
+                newLabel.innerHTML = 'selectionner une couleur :';
+                divSelect.appendChild(newLabel);
 
-                    console.log(data[i]);
+                const newSelect = document.createElement('select');
+                newSelect.setAttribute('class', 'bloc__section_2__select--font');
+                newLabel.appendChild(newSelect);
+
+                for (b = 0; b < data2.colors.length; b++) {
+                    const newOption = document.createElement('option');
+                    newOption.setAttribute('value', '');
+                    newOption.innerHTML = data2.colors[b];
+                    newSelect.appendChild(newOption);
                 };
+
+                console.log(data2[i]);
+
             });
 
         } else {
