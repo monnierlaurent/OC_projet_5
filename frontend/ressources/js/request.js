e1ffb14ecc397d2c9e6bf57ea6004c80e02afd4f
monnierlaurent <laurent.monnier51@orange.fr>
2020-05-15 15:08:31 +0200

    promesse fetch() avec traitemtnt des message d'erreurs + affichage dynamique des produit sur l'index

--- frontend/ressources/js/request.js
+++ frontend/ressources/js/request.js
@@ -1,35 +1,46 @@
-fetch('http://localhost:3000/api/teddies/')
-    .then(function(response) {
-        return response.json();
+let urlJson = 'http://localhost:3000/api/teddies/';
 
-    }).then(function(rep) {
+const listproduits = async function() {
+    let response = await fetch('http://localhost:3000/api/teddies/');
 
-        for (i = 0; i < rep.length; i++) {
+    if (response.ok) {
+        let data = await response.json().then(function(data, i) {
+            for (i = 0; i < data.length; i++) {
 
-            let sectionProduit = document.getElementById('page_items');
+                let sectionProduit = document.getElementById('page_items');
 
-            const newArticle = document.createElement('article');
-            newArticle.setAttribute('class', 'bloc__section__article--border');
-            sectionProduit.appendChild(newArticle);
+                const newArticle = document.createElement('article');
+                newArticle.setAttribute('class', 'bloc__section__article--border');
+                sectionProduit.appendChild(newArticle);
 
-            const newLien = document.createElement('a');
-            newLien.setAttribute('class', 'bloc__section__article--flex');
-            newLien.setAttribute('href', '#');
-            newArticle.appendChild(newLien);
+                const newLien = document.createElement('a');
+                newLien.setAttribute('class', 'bloc__section__article--flex');
+                newLien.setAttribute('href', 'orinoco_page_produit.html#' + data[i]._id);
+                newArticle.appendChild(newLien);
 
-            const newImg = document.createElement('img');
-            newImg.setAttribute('class', 'bloc__section__article--image');
-            newImg.setAttribute('src', rep[i].imageUrl);
-            newLien.appendChild(newImg);
+                const newImg = document.createElement('img');
+                newImg.setAttribute('class', 'bloc__section__article--image');
+                newImg.setAttribute('src', data[i].imageUrl);
+                newLien.appendChild(newImg);
 
-            const newName = document.createElement('h2');
-            newName.setAttribute('class', 'bloc__section__heading--font');
-            newName.setAttribute('src', '/backend/images/teddy_1.jpg');
-            newName.innerHTML = rep[i].name;
-            newLien.appendChild(newName);
+                const newName = document.createElement('h2');
+                newName.setAttribute('class', 'bloc__section__heading--font');
+                newName.innerHTML = data[i].name;
+                newLien.appendChild(newName);
+
+                const newParag = document.createElement('p');
+                newParag.setAttribute('class', 'bloc__section__heading--font');
+                newParag.innerHTML = data[i].price + '€';
+                newLien.appendChild(newParag);
+
+                console.log(data[i]);
+            };
+        });
+
+    } else {
+        console.error('retour du serveur : ', response.status);
+    };
+};
 
 
-
-            console.log(rep[i]);
-        };
-    });
+listproduits();
