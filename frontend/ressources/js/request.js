585b074111ef5d93f80a3497f4ef4ad342380d66
monnierlaurent <laurent.monnier51@orange.fr>
2020-06-20 09:47:33 +0200

    creation du fichier createElements.js pour les function communes

--- frontend/ressources/js/request.js
+++ frontend/ressources/js/request.js
@@ -14,28 +14,3 @@ async function send(data) {
 };
 
 // function de creation d'element
-
-createElm1 = (balise, value, attribut, attributValue) => {
-    const newElm = document.createElement(balise);
-    newElm.setAttribute(attribut, attributValue);
-    newElm.innerHTML = value;
-    return newElm;
-};
-
-createElm2 = (balise, value, attribut1, attributValue1, attribut2, attributValue2) => {
-    const newLabel = document.createElement(balise);
-    newLabel.setAttribute(attribut1, attributValue1);
-    newLabel.setAttribute(attribut2, attributValue2);
-    newLabel.innerHTML = value;
-    return newLabel;
-};
-
-createinputs = (balise, atb1, atbVal1, atb2, atbVal2, atb3, atbVal3, atb4, atbVal4, atb5, atbVal5) => {
-    const newInput = document.createElement(balise);
-    newInput.setAttribute(atb1, atbVal1);
-    newInput.setAttribute(atb2, atbVal2);
-    newInput.setAttribute(atb3, atbVal3);
-    newInput.setAttribute(atb4, atbVal4);
-    newInput.setAttribute(atb5, atbVal5);
-    return newInput;
-};
